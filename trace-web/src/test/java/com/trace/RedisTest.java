5eed2ae2c00ee788e1fb732d2404d17e1712d323
徐泽朋 <2427389569@qq.com>
2021-03-19 22:21:53 +0800

    测试类测试通过

---
+++ trace-web/src/test/java/com/trace/RedisTest.java
@@ -0,0 +1,32 @@
+package com.trace;
+
+import com.trace.controller.TestMysqlUsableController;
+import com.trace.dao.entity.TestMysqlUsable;
+import com.trace.util.Result;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author xzp
+ * Created on 2021/3/19
+ */
+@SpringBootTest(classes = MainApp.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RedisTest {
+    @Autowired
+    TestMysqlUsableController tmu;
+
+    @Test
+    public void test() {
+        System.out.println("test suc");
+        Assert.assertNotNull(tmu);
+        Result result = tmu.testInsertAva();
+        Assert.assertEquals((Integer) 1,result.getCode());
+
+    }
+
+}
