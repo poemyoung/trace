d0a7ba6bff598e7692de9a3a514ea3744956a01f
徐泽朋 <zepeng1.xu@tcl.com>
2021-03-26 19:30:00 +0800

    新建图片保存工具类

---
+++ trace-common/src/main/java/com/common/utils/ImageUtil.java
@@ -0,0 +1,16 @@
+package com.common.utils;
+
+/**
+ * @author xzp
+ * Created on 2021/3/26
+ */
+// unfinished
+public class ImageUtil {
+    private static final String BASE_PATH = "/Users/xzp/Desktop/upload";
+    private static final String ADMIN_IMG_PATH = "/Users/xzp/Desktop/upload/admin";
+
+    public static String imageLoad() {
+
+        return null;
+    }
+}
