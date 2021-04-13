4be791cc91c86404e21c6460c730b4e6ce8e25c5
徐泽朋 <zepeng1.xu@tcl.com>
2021-04-13 12:41:21 +0800

    冷链二维码生成

--- trace-service/src/main/java/com/trace/service/coldchain/ColdChainService.java
+++ trace-service/src/main/java/com/trace/service/coldchain/ColdChainService.java
@@ -1,9 +1,21 @@
 package com.trace.service.coldchain;
 
+import com.common.enums.Colors;
+import com.common.utils.QRCode;
+import com.common.utils.SnowflakeIdUtil;
+import com.trace.dao.entity.ColdChain;
 import com.trace.dao.repository.ColdChainMapper;
 import com.trace.service.entity.recentity.ColdChainBaseMsg;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * @author xzp
@@ -11,13 +23,45 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ColdChainService {
+    private final Logger LOGGER = LoggerFactory.getLogger(ColdChainService.class);
 
     @Autowired
     ColdChainMapper ccMapper;
 
+    @Value("${qrcode.path}")
+    private String path = "/Users/xzp/Desktop/upload";
+
+    SnowflakeIdUtil snow = new SnowflakeIdUtil();
+
     public String declareAColdChain(ColdChainBaseMsg msg) {
         // 插入冷链，生成冷链二维码、插入地址信息（单独分出来）
+        ColdChain coldChain = new ColdChain();
+        int id = snow.nextIntId();
+        coldChain.setIdcoldchain(id);
+        coldChain.setClassify(msg.getClassify());
+        coldChain.setRemark(msg.getClassify());
+        coldChain.setSource(msg.getCompany());
+        // 生成二维码返回路径
+        String s = this.generateChainQrCode(id);
+        if(StringUtils.isBlank(s)) {
+            return "";
+        }
+        coldChain.setQrpath(s);
+        // 插入数据库中
+        int i = ccMapper.insertSelective(coldChain);
+        return s;
+    }
 
-        return "";
+    private String generateChainQrCode(Integer id) {
+        BufferedImage image = QRCode.createImageByColor(id+"", Colors.DEFAULT);
+        String tag = "/" + id + ".jpg";
+        try {
+            File f = new File(path + tag);
+            ImageIO.write(image,"jpg",f);
+        }catch (Exception e) {
+            LOGGER.error("图片写出错误！");
+            return "";
+        }
+        return tag;
     }
 }
